558fb11d3d3470c57174cf828e30616216bc5ae2
Hoang-k68a3hus <179712404+Hoang-k68a3hus@users.noreply.github.com>
2026-08-08 20:57:04 +0700

    Export canonical document assembler

--- source_understanding/__init__.py
+++ source_understanding/__init__.py
@@ -1,1 +1,15 @@
 """Universal source-understanding package."""
+
+from .assembly import (
+    ASSEMBLY_VERSION,
+    STRUCTURE_PIPELINE_VERSION,
+    AssemblyError,
+    CanonicalDocumentAssembler,
+)
+
+__all__ = [
+    "ASSEMBLY_VERSION",
+    "STRUCTURE_PIPELINE_VERSION",
+    "AssemblyError",
+    "CanonicalDocumentAssembler",
+]
