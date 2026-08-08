b0fd7a14ff54790bc5940fd544e29b073326e68e
Hoang-k68a3hus <179712404+Hoang-k68a3hus@users.noreply.github.com>
2026-08-08 20:58:45 +0700

    Keep source understanding root package lightweight

--- source_understanding/__init__.py
+++ source_understanding/__init__.py
@@ -1,15 +1,1 @@
 """Universal source-understanding package."""
-
-from .assembly import (
-    ASSEMBLY_VERSION,
-    STRUCTURE_PIPELINE_VERSION,
-    AssemblyError,
-    CanonicalDocumentAssembler,
-)
-
-__all__ = [
-    "ASSEMBLY_VERSION",
-    "STRUCTURE_PIPELINE_VERSION",
-    "AssemblyError",
-    "CanonicalDocumentAssembler",
-]
